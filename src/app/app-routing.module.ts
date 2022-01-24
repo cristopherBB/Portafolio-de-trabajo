b50aaf81d1b0fbaaa2de2a7ec9ea7ad1f33ebb27
cristopherBB <38765189+cristopherBB@users.noreply.github.com>
2022-01-23 23:34:29 -0400

    agregado header de la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BriefcaseComponent } from './briefcase/briefcase.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    redirectTo: '/briefcase',
+    pathMatch: 'full', 
+  },
+
+  {
+    path: 'briefcase',
+    component: BriefcaseComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BriefcaseComponent } from './briefcase/briefcase.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './shared-components/navbar/navbar.component';
+
+// Angular material
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BriefcaseComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
