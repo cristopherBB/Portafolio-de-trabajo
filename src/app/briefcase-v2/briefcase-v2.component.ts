e096aab081533457db6d29ab57e58f512dc8bef4
cristopherBB <38765189+cristopherBB@users.noreply.github.com>
2022-03-04 16:58:36 -0400

    update
    -Se agregaron animaciones a los elementos de la pagina

--- src/app/briefcase-v2/briefcase-v2.component.ts
+++ src/app/briefcase-v2/briefcase-v2.component.ts
@@ -13,3 +13,26 @@ export class BriefcaseV2Component implements OnInit {
   }
 
 }
+
+function reveal() {
+
+  animate(".textAbout",10, "animate__fadeInUp")
+  animate(".works",30, "animate__fadeInLeft")
+  animate(".github",30, "animate__fadeInUp")
+  animate(".contact_list", 30, "animate__fadeInRight")
+
+}
+
+function animate(classType: string, visible: number, efect: string){
+  var reveals = document.querySelector(classType);
+  var windowHeight = window.innerHeight;
+  var elementTop = reveals!.getBoundingClientRect().top;
+  var elementVisible = visible;
+  if (elementTop < windowHeight - elementVisible) {
+    reveals!.classList.add("animate__animated", efect);
+  } else {
+    reveals!.classList.remove("animate__animated", efect);
+  }
+}
+
+window.addEventListener("scroll", reveal);
